1251d26240269eeb990da679f3e3ea2cc15bcc1d
Siddhant Yadav <siddthecoder@gmail.com>
2024-10-21 16:01:58 +0545

    The Firts Commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        happyMonkey: ['"Happy Monkey"', 'cursive'],
+        silkscreen: ['"Silkscreen"', 'cursive'], // Add the new font family
+      },
+      backgroundImage: {
+        'hero-pattern': "url('./asset/mmbg (1).png')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      },
+    },
+  },
+  plugins: [],
+}
+
